053c1256582944d34987260414b17c49c3bb4481
tazodvalishvili <tdval19@freeuni.edu.ge>
2022-01-30 14:16:59 +0400

    Endpoints added

--- app/infra/fastapi/test.py
+++ app/infra/fastapi/test.py
@@ -9,3 +9,43 @@ api = APIRouter()
 @api.get("/test")
 def test(core: TestService = Depends(get_core)) -> str:
     return core.hello()
+
+
+@api.get("/wallets")
+def get_wallets(core: TestService = Depends(get_core)) -> str:
+    pass
+
+
+@api.get("/transactions")
+def get_transactions(core: TestService = Depends(get_core)) -> str:
+    pass
+
+
+@api.get("/wallets/{address}/transactions")
+def get_address_transactions(core: TestService = Depends(get_core)) -> str:
+    pass
+
+
+@api.get("/statistics")
+def get_statistics(core: TestService = Depends(get_core)) -> str:
+    pass
+
+
+@api.post("/users")
+def post_users(core: TestService = Depends(get_core)) -> str:
+    pass
+
+
+@api.post("/wallets")
+def post_wallets(core: TestService = Depends(get_core)) -> str:
+    pass
+
+
+@api.post("/transactions")
+def post_transactions(core: TestService = Depends(get_core)) -> str:
+    pass
+
+
+@api.post("/users")
+def post_users(core: TestService = Depends(get_core)) -> str:
+    pass
