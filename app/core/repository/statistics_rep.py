0d1ee719732b33649993805619653ef141c8628d
99559 <lkala19@freeuni.edu.ge>
2022-02-02 22:28:45 +0400

    deleted temp table set up

--- app/core/repository/statistics_rep.py
+++ app/core/repository/statistics_rep.py
@@ -38,25 +38,3 @@ class StatisticsDBRepository(IStatisticRepository):
         return api_key == ADMIN_KEY
 
 
-class set_up_db_staff:
-
-    def __init__(self) -> None:
-        self.connection = sqlite3.connect('project_db')
-        self.cursor = self.connection.cursor()
-        self.create_tables()
-
-    def create_tables(self) -> None:
-        create_user_table = '''CREATE TABLE IF NOT EXISTS user_table(user_id INTEGER PRIMARY KEY AUTOINCREMENT)'''
-        self.cursor.execute(create_user_table)
-
-        create_wallet_table = '''CREATE TABLE IF NOT EXISTS wallet_table
-                                    (wallet_address INTEGER PRIMARY KEY AUTOINCREMENT, user_id,
-                                    FOREIGN KEY (user_id)  REFERENCES user_table (user_id))'''
-        self.cursor.execute(create_wallet_table)
-
-        create_transaction_table = ''' CREATE TABLE IF NOT EXISTS transaction_table
-                                        (transaction_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                        from_wallet_address, to_wallet_address, bitcoin_quantity INTEGER,
-                                        FOREIGN KEY (from_wallet_address) REFERENCES wallet_table (wallet_address),
-                                        FOREIGN KEY (to_wallet_address) REFERENCES wallet_table (wallet_address))'''
-        self.cursor.execute(create_transaction_table)
