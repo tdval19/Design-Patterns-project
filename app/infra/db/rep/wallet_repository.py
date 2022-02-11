3d028a60016c7eec73124261258da7a24e4c876f
AkakiZZ <zghuladze16@gmail.com>
2022-02-11 15:41:35 +0400

    updated wallet_repository.py

--- app/infra/db/rep/wallet_repository.py
+++ app/infra/db/rep/wallet_repository.py
@@ -1,5 +1,6 @@
 import sqlite3
 from dataclasses import dataclass
+from sqlite3 import Connection
 from typing import Optional
 
 from app.core.models.wallet import Wallet
@@ -8,38 +9,31 @@ from app.core.repository.repository_interfaces import IWalletRepository
 
 @dataclass
 class SqlWalletRepository(IWalletRepository):
-    def __init__(self, db_name: str) -> None:
-        self.db_name = db_name
+    con: Connection
 
     def get_by_address(self, wallet_address: int) -> Optional[Wallet]:
-        con = sqlite3.connect(self.db_name)
         statement = "SELECT user_id, balance_btc, wallet_address FROM  wallet_table WHERE wallet_address = ?"
-        cursor = con.cursor()
+        cursor = self.con.cursor()
         cursor.execute(statement, (wallet_address,))
         rows = cursor.fetchall()
-        con.commit()
+        self.con.commit()
         cursor.close()
-        con.close()
         if len(rows) == 0:
             return None
         return Wallet(rows[0][0], rows[0][1], rows[0][2])
 
     def update_wallet_balance(self, wallet_address: int, balance_btc: float) -> None:
-        con = sqlite3.connect(self.db_name)
         statement = "UPDATE wallet_table SET balance_btc=(?) WHERE wallet_address=?;"
-        cursor = con.cursor()
+        cursor = self.con.cursor()
         cursor.execute(statement, (balance_btc, wallet_address))
-        con.commit()
+        self.con.commit()
         cursor.close()
-        con.close()
 
     def add(self, wallet: Wallet) -> None:
-        con = sqlite3.connect(self.db_name)
         statement = "INSERT INTO wallet_table(wallet_address, user_id, balance_btc) VALUES (?, ?, ?)"
-        cursor = con.cursor()
+        cursor = self.con.cursor()
         cursor.execute(
             statement, (wallet.wallet_id, wallet.user_id, wallet.balance_btc)
         )
-        con.commit()
+        self.con.commit()
         cursor.close()
-        con.close()
