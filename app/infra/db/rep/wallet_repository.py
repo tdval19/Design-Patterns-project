846f78e8e501e8f6ebad4e227ce8a64cafbffeeb
tazodvalishvili <tdval19@freeuni.edu.ge>
2022-02-11 21:59:03 +0400

    Added transaction repository. Updated wallet repository.

--- app/infra/db/rep/wallet_repository.py
+++ app/infra/db/rep/wallet_repository.py
@@ -29,14 +29,30 @@ class SqlWalletRepository(IWalletRepository):
         self.con.commit()
         cursor.close()
 
-    def add(self, wallet: Wallet) -> None:
-        statement = "INSERT INTO wallet_table(wallet_address, user_id, balance_btc) VALUES (?, ?, ?)"
+    def add(self, wallet: Wallet) -> Wallet:
+        statement = "INSERT INTO wallet_table(user_id, balance_btc) VALUES (?, ?)"
         cursor = self.con.cursor()
         cursor.execute(
-            statement, (wallet.wallet_id, wallet.user_id, wallet.balance_btc)
+            statement, (wallet.user_id, wallet.balance_btc)
         )
         self.con.commit()
+        statement = "SELECT last_insert_rowid()"
+        cursor.execute(statement)
+        rows = cursor.fetchall()
+        self.con.commit()
         cursor.close()
+        wallet_address = rows[0][0]
+        print(wallet_address)
+        return Wallet(wallet.user_id, wallet.balance_btc, wallet_address)
 
     def get_wallets_by_user_id(self, user_id: int) -> List[Wallet]:
-        pass
+        statement = "SELECT balance_btc, wallet_address FROM wallet_table where user_id = ?";
+        cursor = self.con.cursor()
+        cursor.execute(statement, (user_id,))
+        wallets = cursor.fetchall()
+        res_list: List[Wallet] = []
+        for wallet in wallets:
+            wallet = Wallet(user_id, wallet[0], wallet[1])
+            res_list.append(wallet)
+
+        return res_list
--- app/test/test_sql_wallet_repository.py
+++ app/test/test_sql_wallet_repository.py
@@ -16,21 +16,21 @@ def repository(get_db_script_path: Path) -> SqlWalletRepository:
 
 class TestSqlWalletRepository:
     def test_add_get_by_address(self, repository: SqlWalletRepository) -> None:
-        wallet = Wallet(1, 5.12, 2)
-        repository.add(wallet)
-        get_wallet = repository.get_by_address(2)
-        assert wallet == get_wallet
-        wallet = Wallet(5, 136.7, 3)
-        repository.add(wallet)
-        get_wallet = repository.get_by_address(3)
-        assert wallet == get_wallet
+        wallet = Wallet(1, 5.12)
+        added_wallet = repository.add(wallet)
+        get_wallet = repository.get_by_address(added_wallet.wallet_id)
+        assert added_wallet == get_wallet
+        wallet = Wallet(5, 136.7)
+        added_wallet = repository.add(wallet)
+        get_wallet = repository.get_by_address(added_wallet.wallet_id)
+        assert added_wallet == get_wallet
 
     def test_update_balance(self, repository: SqlWalletRepository) -> None:
-        wallet = Wallet(5, 100, 10)
-        repository.add(wallet)
-        get_wallet = repository.get_by_address(10)
-        assert wallet == get_wallet
-        repository.update_wallet_balance(10, 200)
-        expected_wallet = Wallet(5, 200, 10)
-        assert repository.get_by_address(10) == expected_wallet
+        wallet = Wallet(5, 100)
+        added_wallet = repository.add(wallet)
+        get_wallet = repository.get_by_address(added_wallet.wallet_id)
+        assert added_wallet == get_wallet
+        repository.update_wallet_balance(added_wallet.wallet_id, 200)
+        expected_wallet = Wallet(5, 200, added_wallet.wallet_id)
+        assert repository.get_by_address(added_wallet.wallet_id) == expected_wallet
 
