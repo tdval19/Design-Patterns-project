23f7ada5ef4de3751513d8325e6447be8eeeea79
99559 <lkala19@freeuni.edu.ge>
2022-02-12 13:53:19 +0400

    added new wall repo func test

--- app/test/test_sql_wallet_repository.py
+++ app/test/test_sql_wallet_repository.py
@@ -33,3 +33,23 @@ class TestSqlWalletRepository:
         repository.update_wallet_balance(added_wallet.wallet_address, 200)
         expected_wallet = Wallet(5, 200, added_wallet.wallet_address)
         assert repository.get_by_address(added_wallet.wallet_address) == expected_wallet
+
+    def test_get_user_wallets(self, repository: SqlWalletRepository) -> None:
+        wallet1 = Wallet(1, 5.12)
+        wallet2 = Wallet(1, 3.8)
+        wallet3 = Wallet(1, 4.9)
+
+        first_added = repository.add(wallet1)
+        user_wallets1 = repository.get_wallets_by_user_id(1)
+        assert(user_wallets1[0] == first_added)
+
+        second_added = repository.add(wallet2)
+        user_wallets2 = repository.get_wallets_by_user_id(1)
+        assert (user_wallets2[0] == first_added)
+        assert (user_wallets2[1] == second_added)
+
+        third_added = repository.add(wallet3)
+        user_wallets3 = repository.get_wallets_by_user_id(1)
+        assert (user_wallets3[0] == first_added)
+        assert (user_wallets3[1] == second_added)
+        assert (user_wallets3[2] == third_added)
