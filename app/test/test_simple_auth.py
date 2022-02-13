a66f5ddd897770971e23286810b5a886ec2096d6
99559 <lkala19@freeuni.edu.ge>
2022-02-13 22:25:29 +0400

    added simple auth tests

--- app/test/test_simple_auth.py
+++ app/test/test_simple_auth.py
@@ -23,19 +23,44 @@ class TestSimpleAuth:
     def test_generate_key(self, connection: sqlite3.Connection):
         user_repo = SqlUserRepository(connection)
         simple_auth = SimpleAuth(user_repo, SimpleKeyEncoder())
-        pass
+        credentials = UserCredentials(1)
+        assert isinstance(simple_auth.generate_key(credentials), str)
 
     def test_authorise_user(self, connection: sqlite3.Connection):
         user_repo = SqlUserRepository(connection)
         simple_auth = SimpleAuth(user_repo, SimpleKeyEncoder())
-        pass
+        with pytest.raises(NotAuthorizedException):
+            simple_auth.authorize_user(None)
+        with pytest.raises(NotAuthorizedException):
+            simple_auth.authorize_user("KEY_TEST")
+        credentials = UserCredentials(1)
+        key = simple_auth.generate_key(credentials)
+        with pytest.raises(NotAuthorizedException):
+            simple_auth.authorize_user(key)
+        user = user_repo.add(User())
+        credentials = UserCredentials(user.user_id)
+        key = simple_auth.generate_key(credentials)
+        assert simple_auth.authorize_user(key) == credentials
 
     def test_user_has_permission(self, connection: sqlite3.Connection):
         user_repo = SqlUserRepository(connection)
         simple_auth = SimpleAuth(user_repo, SimpleKeyEncoder())
-        pass
+
+        with pytest.raises(NotAuthorizedException):
+            simple_auth.user_has_permission(None, 5)
+
+        with pytest.raises(NotAuthorizedException):
+            simple_auth.user_has_permission("SOME_USER_KEY", 6)
+
+        with pytest.raises(ForbiddenException):
+            simple_auth.user_has_permission("KEY6", 7)
 
     def test_authorise_admin(self, connection: sqlite3.Connection):
         user_repo = SqlUserRepository(connection)
         simple_auth = SimpleAuth(user_repo, SimpleKeyEncoder())
-        pass
+        # test key is non
+        with pytest.raises(NotAuthorizedException):
+            simple_auth.authorize_admin(None)
+        # test not authorised
+        with pytest.raises(NotAuthorizedException):
+            simple_auth.authorize_admin("NON_ADMIN_KEY")
