b496de786349717c88c72aba0b6ce5ea9ca24795
tazodvalishvili <tdval19@freeuni.edu.ge>
2022-02-13 23:41:32 +0400

    Added statistic repository tests.

--- app/test/test_sql_statistic_repository.py
+++ app/test/test_sql_statistic_repository.py
@@ -1,58 +1,43 @@
-# import random
-# from pathlib import Path
-# from typing import Tuple
-#
-# import pytest
-#
-# from app.infra.db.init_db import SqliteDbInitializer
-# from app.infra.db.rep.statistic_repository import SqlStatisticRepository
-# from app.test.db_path_fixture import get_db_script_path
-#
-#
-# @pytest.fixture
-# def repository(get_db_script_path: Path) -> SqlStatisticRepository:
-#     db = SqliteDbInitializer(get_db_script_path, ":memory:")
-#     return SqlStatisticRepository(db.get_connection())
-#
-#
-# @pytest.fixture
-# def example_update() -> Tuple[int, float]:
-#     return 1, 5
-#
-#
-# class TestSqlStatisticRepository:
-#     def test_should_return_zeroes(self, repository: SqlStatisticRepository) -> None:
-#         stats = repository.get()
-#         assert stats.total_profit == 0.0
-#         assert stats.num_of_transactions == 0
-#
-#     def test_should_update_values(
-#         self, repository: SqlStatisticRepository, example_update: Tuple[int, float]
-#     ) -> None:
-#         before_stats = repository.get()
-#         delta_transactions, delta_profit = example_update
-#         repository.update(delta_transactions, delta_profit)
-#         after_stats = repository.get()
-#         assert (
-#             after_stats.num_of_transactions - before_stats.num_of_transactions
-#             == delta_transactions
-#         )
-#         assert after_stats.total_profit - before_stats.total_profit == delta_profit
-#
-#     def test_should_update_values_with_random(
-#         self, repository: SqlStatisticRepository
-#     ) -> None:
-#         for i in range(20):
-#             before_stats = repository.get()
-#             delta_profit = round(random.random(), 3)
-#             delta_transactions = 1
-#             repository.update(delta_transactions, delta_profit)
-#             after_stats = repository.get()
-#             assert (
-#                 after_stats.num_of_transactions - before_stats.num_of_transactions
-#                 == delta_transactions
-#             )
-#             assert (
-#                 after_stats.total_profit - before_stats.total_profit
-#                 == pytest.approx(delta_profit)
-#             )
+from pathlib import Path
+from sqlite3 import Connection
+
+import pytest
+
+from app.core.models.transaction import Transaction
+from app.infra.db.init_db import SqliteDbInitializer
+from app.infra.db.rep.statistic_repository import SqlStatisticRepository
+from app.infra.db.rep.transaction_repository import SqlTransactionRepository
+
+
+@pytest.fixture
+def db_fixture(get_db_script_path: Path) -> Connection:
+    db = SqliteDbInitializer(get_db_script_path, ":memory:")
+    return db.get_connection()
+
+
+@pytest.fixture
+def repository(db_fixture: Connection) -> SqlStatisticRepository:
+    return SqlStatisticRepository(db_fixture)
+
+
+@pytest.fixture
+def transaction_repository(db_fixture: Connection) -> SqlTransactionRepository:
+    return SqlTransactionRepository(db_fixture)
+
+
+class TestSqlStatisticRepository:
+    def test_statistic_repository_empty(self, repository: SqlStatisticRepository):
+        repository.get().total_profit = 0
+        repository.get().num_of_transactions = 0
+
+    def test_statistic_repository_get(self, repository: SqlStatisticRepository,
+                                      transaction_repository: SqlTransactionRepository):
+        transaction_one = Transaction(1, 2, 5.00, 1.00, 1)
+        transaction_two = Transaction(3, 4, 10.50, 2.00, 2)
+        transaction_three = Transaction(1, 3, 3.46, 3.50, 3)
+        transaction_repository.add(transaction_one)
+        transaction_repository.add(transaction_two)
+        transaction_repository.add(transaction_three)
+        stats = repository.get()
+        assert stats.num_of_transactions == 3
+        assert stats.total_profit == 6.50
