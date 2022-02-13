f8c461054d054430cbca97cfec0b1a2d9d1c0b03
AkakiZZ <zghuladze16@gmail.com>
2022-02-13 19:46:51 +0400

    prototype with auth

--- app/core/interactors/wallets.py
+++ app/core/interactors/wallets.py
@@ -8,6 +8,8 @@ from app.core.repository.repository_interfaces import IWalletRepository
 @dataclass
 class WalletInteractor:
     repository: IWalletRepository
+    num_wallet_limit: int = 3
+    initial_balance_btc: int = 1
 
     def get_wallet(self, address: int) -> Wallet:
         result_wallet = self.repository.get_by_address(address)
@@ -17,9 +19,9 @@ class WalletInteractor:
 
     def create_wallet(self, user_id: int) -> Wallet:
         user_wallets = self.repository.get_wallets_by_user_id(user_id)
-        if len(user_wallets) >= 3:
+        if len(user_wallets) >= self.num_wallet_limit:
             raise UserReachedWalletLimitException(user_id)
-        add_wallet = Wallet(user_id, 1.0)
+        add_wallet = Wallet(user_id, self.initial_balance_btc)
         return self.repository.add(add_wallet)
 
 
--- app/runner/setup.py
+++ app/runner/setup.py
@@ -4,21 +4,54 @@ from fastapi import FastAPI
 
 from app.core.converter.bitcoin_converter import CexBitcoinConverter
 from app.core.facade import BitcoinService
+from app.core.interactors.auth import (
+    SimpleAuth,
+    NotAuthorizedException,
+    ForbiddenException,
+)
 from app.core.interactors.statistics import StatisticInteractor
-from app.core.interactors.transactions import TransactionsInteractor
-from app.core.interactors.users import UserInteractor
-from app.core.interactors.wallets import WalletInteractor
+from app.core.interactors.transactions import (
+    TransactionsInteractor,
+    NotEnoughBitcoinsException,
+)
+from app.core.interactors.users import UserInteractor, UserNotFoundException
+from app.core.interactors.wallets import (
+    WalletInteractor,
+    WalletNotFoundException,
+    UserReachedWalletLimitException,
+)
 from app.infra.db.init_db import SqliteDbInitializer
 from app.infra.db.rep.statistic_repository import SqlStatisticRepository
 from app.infra.db.rep.transaction_repository import SqlTransactionRepository
 from app.infra.db.rep.user_repository import SqlUserRepository
 from app.infra.db.rep.wallet_repository import SqlWalletRepository
+from app.infra.encoder.dummy_encoder import SimpleKeyEncoder
 from app.infra.fastapi.api import api
+from app.infra.fastapi.api_exception_handlers import (
+    user_not_found_exception_handler,
+    wallet_not_found_exception_handler,
+    not_authorized_exception_handler,
+    forbidden_access_exception_handler,
+    not_enough_bitcoins_exception_handler,
+    user_reached_wallet_limit_exception_handler,
+)
 
 
 def setup() -> FastAPI:
     app = FastAPI()
     app.include_router(api)
+    app.add_exception_handler(UserNotFoundException, user_not_found_exception_handler)
+    app.add_exception_handler(
+        WalletNotFoundException, wallet_not_found_exception_handler
+    )
+    app.add_exception_handler(NotAuthorizedException, not_authorized_exception_handler)
+    app.add_exception_handler(ForbiddenException, forbidden_access_exception_handler)
+    app.add_exception_handler(
+        NotEnoughBitcoinsException, not_enough_bitcoins_exception_handler
+    )
+    app.add_exception_handler(
+        UserReachedWalletLimitException, user_reached_wallet_limit_exception_handler
+    )
 
     db_path = Path(__file__).parent / "../infra/db/bitcoin_wallet.db"
     script_path = Path(__file__).parent / "../infra/db/scheme.sql"
@@ -27,6 +60,7 @@ def setup() -> FastAPI:
     statistics_rep = SqlStatisticRepository(db.get_connection())
     transaction_rep = SqlTransactionRepository(db.get_connection())
     user_rep = SqlUserRepository(db.get_connection())
+    encoder = SimpleKeyEncoder()
 
     app.state.core = BitcoinService(
         StatisticInteractor(statistics_rep),
@@ -34,5 +68,6 @@ def setup() -> FastAPI:
         TransactionsInteractor(transaction_rep, wallet_rep),
         UserInteractor(user_rep),
         CexBitcoinConverter(),
+        SimpleAuth(user_rep, encoder),
     )
     return app
